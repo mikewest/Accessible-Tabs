e0b129cfae4f3456b2433671780dd52b9311709e
Mike West <mike@mikewest.org>
2009-05-02 23:17:16 +0200

    Whitespace changes.  It drives me nuts to have multiple commands on the same line, especially when the line's long enough to push important info off the side of my screen.
    Next step, making the inline HTML a little more readable.  It'd be nice if jQuery had something like YUI's `substitute` built in.  *sigh*

--- js/jquery.tabs.js
+++ js/jquery.tabs.js
@@ -25,75 +25,87 @@
  * * * added clearfix and tabcounter that adds a class in the schema "tabamount{number amount of tabs}" to the ul containg the tabs so one can style the tabs to fit 100% into the width
  */
 
-(function($) {
+( function( $ ) {
     
-    $.fn.extend({
-        getUniqueId: function(p){
+    $.fn.extend( {
+        getUniqueId: function( p ){
             return p + new Date().getTime();
         },
-        accessibleTabs: function(config) {
+        accessibleTabs: function( config ) {
             var defaults = {
-                wrapperClass: 'content', // Classname to apply to the div that is wrapped around the original Markup
-                currentClass: 'current', // Classname to apply to the LI of the selected Tab
-                tabhead: 'h4', // Tag or valid Query Selector of the Elements to Transform the Tabs-Navigation from (originals are removed)
-                tabbody: '.tabbody', // Tag or valid Query Selector of the Elements to be treated as the Tab Body
-                fx:'show', // can be "fadeIn", "slideDown", "show"
-                fxspeed: 'normal', // speed (String|Number): "slow", "normal", or "fast") or the number of milliseconds to run the animation
-                currentInfoText: 'current tab: ', // text to indicate for screenreaders which tab is the current one
-                currentInfoPosition: 'prepend', // Definition where to insert the Info Text. Can be either "prepend" or "append"
-                currentInfoClass: 'current-info', // Class to apply to the span wrapping the CurrentInfoText
-                tabsListClass:'tabs-list', // Class to apply to the generated list of tabs above the content
-                syncheights:false // syncs the heights of the tabs when the SyncHeight plugin is available http://blog.ginader.de/dev/jquery/syncheight/index.php
+                wrapperClass: 			'content', 			// Classname to apply to the div that is wrapped around the original Markup
+                currentClass:       	'current', 			// Classname to apply to the LI of the selected Tab
+                tabhead:            	'h4', 				// Tag or valid Query Selector of the Elements to Transform the Tabs-Navigation from (originals are removed)
+                tabbody:           		'.tabbody', 		// Tag or valid Query Selector of the Elements to be treated as the Tab Body
+                fx:                 	'show', 			// can be "fadeIn", "slideDown", "show"
+                fxspeed:            	'normal', 			// speed (String|Number): "slow", "normal", or "fast") or the number of milliseconds to run the animation
+                currentInfoText:     	'current tab: ',	// text to indicate for screenreaders which tab is the current one
+                currentInfoPosition:    'prepend', 			// Definition where to insert the Info Text. Can be either "prepend" or "append"
+                currentInfoClass:       'current-info', 	// Class to apply to the span wrapping the CurrentInfoText
+                tabsListClass:          'tabs-list', 		// Class to apply to the generated list of tabs above the content
+                syncheights:            false 				// syncs the heights of the tabs when the SyncHeight plugin is available http://blog.ginader.de/dev/jquery/syncheight/index.php
             };
-            var options = $.extend(defaults, config);
-            var o = this;
-            return this.each(function() {
-                var el = $(this);
-                var list = '';
-                var tabCount = 0;
+            var options	= $.extend( defaults, config );
+            var o		= this;
+            return this.each( function( ) {
+                var el			= $( this ),
+                	list		= '',
+                	tabCount 	= 0;
 
-                var contentAnchor = o.getUniqueId('accessibletabscontent');
-                var tabsAnchor = o.getUniqueId('accessibletabs');
-                $(el).wrapInner('<div class="'+options.wrapperClass+'"></div>');
+                var contentAnchor 	= o.getUniqueId( 'accessibletabscontent' ),
+               		tabsAnchor		= o.getUniqueId( 'accessibletabs' );
+                
+				el.wrapInner( '<div class="' + options.wrapperClass + '"></div>' );
 
-                $(el).find(options.tabhead).each(function(i){
-                    var id = '';
-                    if(i === 0){
-                        id =' id="'+tabsAnchor+'"';
+                el.find( options.tabhead ).each( function( i ) {
+                    var id	= '',
+						el	= $( this );
+						
+                    if ( i === 0 ) {
+                        id = ' id="' + tabsAnchor + '"';
                     }
-                    list += '<li><a'+id+' href="#'+contentAnchor+'">'+$(this).text()+'</a></li>';
-                    $(this).remove();
+
+                    list += '<li><a' + id + ' href="#' + contentAnchor + '">' + el.text() + '</a></li>';
+                    el.remove();
                     tabCount++;
-                });
+                } );
 
-                $(el).prepend('<ul class="clearfix '+options.tabsListClass+' tabamount'+tabCount+'">'+list+'</ul>');
-                $(el).find(options.tabbody).hide();
-                $(el).find(options.tabbody+':first').show().before('<'+options.tabhead+'><a tabindex="0" class="accessibletabsanchor" name="'+contentAnchor+'" id="'+contentAnchor+'">'+$(el).find("ul>li:first").text()+'</a></'+options.tabhead+'>');
-                $(el).find("ul>li:first").addClass(options.currentClass)
-                .find('a')[options.currentInfoPosition]('<span class="'+options.currentInfoClass+'">'+options.currentInfoText+'</span>');
+                el.prepend( '<ul class="clearfix ' + options.tabsListClass + ' tabamount' + tabCount + '">' + list + '</ul>' );
+                el.find( options.tabbody ).hide();
+                el.find( options.tabbody + ':first' )
+                  .show()
+                  .before( '<' + options.tabhead + '><a tabindex="0" class="accessibletabsanchor" name="' + contentAnchor + '" id="' + contentAnchor + '">' + el.find( "ul>li:first" ).text() + '</a></' + options.tabhead + '>' );
+                el.find( "ul>li:first" )
+                  .addClass( options.currentClass )
+                  .find( 'a' )[ options.currentInfoPosition ]( '<span class="' + options.currentInfoClass + '">' + options.currentInfoText + '</span>' );
 
-                if (options.syncheights && $.fn.syncHeight) {
-                    $(el).find(options.tabbody).syncHeight();
-                    $(window).resize(function(){ 
-                        $(el).find(options.tabbody).syncHeight();
-                    });
+                if ( options.syncheights && $.fn.syncHeight ) {
+                    el.find( options.tabbody )
+                      .syncHeight();
+                    $( window ).resize( function() { 
+                        el.find( options.tabbody )
+                          .syncHeight();
+                    } );
                 }
 
-                $(el).find('ul.'+options.tabsListClass+'>li>a').each(function(i){
-                    $(this).click(function(event){
+                el.find( 'ul.' + options.tabsListClass + '>li>a' ).each( function( i ) {
+                    $( this ).click( function( event ) {
                         event.preventDefault();
-                        $(el).find('ul>li.'+options.currentClass).removeClass(options.currentClass)
-                        .find("span."+options.currentInfoClass).remove();
-                        $(this).blur();
-                        $(el).find(options.tabbody+':visible').hide();
-                        $(el).find(options.tabbody).eq(i)[options.fx](options.fxspeed);
-                        $( '#'+contentAnchor ).text( $(this).text() ).focus();
-                        $(this)[options.currentInfoPosition]('<span class="'+options.currentInfoClass+'">'+options.currentInfoText+'</span>')
-                        .parent().addClass(options.currentClass);
-                        
+                        el.find( 'ul>li.' + options.currentClass )
+                          .removeClass( options.currentClass )
+                          .find("span." + options.currentInfoClass )
+                          .remove();
+                        $( this ).blur();
+                        el.find( options.tabbody + ':visible' )
+                          .hide();
+                        el.find( options.tabbody )
+                          .eq( i )[ options.fx ]( options.fxspeed );
+                        $( '#'+contentAnchor ).text( $( this ).text() ).focus();
+                        $( this )[ options.currentInfoPosition ]( '<span class="' + options.currentInfoClass + '">' + options.currentInfoText + '</span>')
+                          .parent().addClass( options.currentClass );
                     });
                 });
             });
         }
     });
-})(jQuery);
+}( jQuery ) );
